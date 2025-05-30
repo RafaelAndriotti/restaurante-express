29c3c6b230db54629936f9e7d4c590e101f65d5a
Rafael Andriotti <rafael.rebelo47@gmail.com>
2025-05-30 09:55:26 -0300

    Add files via upload
    Arquivo atualizado no computador da prefeitura.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os #biblioteca utilizada para interagir com o sistema operacional subjacente, permitindo que o código manipule arquivos, diretorios, processos, variáveis de ambiente e obtenha informações sobre o sistema.
 
-restaurantes = ['Pizza', 'Sushi', 'Salgado', 'Churrasco'] #lista de todos os restaurantes que serão cadastrados.
+#implementado dicionário para melhor visualização dos cadastros
+restaurantes = [{'nome': 'praça', 'categoria':'Japonesa','ativo':False},
+                {'nome': 'Pizza suprema', 'categoria': 'Italiana', 'ativo': True},
+                {'nome': 'Cantina', 'categoria': 'italiano', 'ativo': False}] #lista de todos os restaurantes que serão cadastrados.
 
 def exibirNomePrograma(): #Função responsável por exibir o nome do programa
     print('Sabor express\n ')
@@ -29,16 +32,39 @@ def exibir_substitulo(texto): #Criado para facilitar no entendimento do código,
 def cadastra_restaurante(): #função criada para cadastrar os restaurantes, onde informamos o nome do mesmo e adicionamos a lista 'restaurantes', logo após podemos digitar qualquer tecla e voltar ao  inicio do sistema.
     exibir_substitulo('Cadastro de novos restaurantes')
     nomeRestaurante = input('Informe o nome do restaurante que a ser cadastrado: ')
-    restaurantes.append(nomeRestaurante)#Adicionar informação a lista
+    categoria = input(f'Digite o nome da categoria do restaurante {nomeRestaurante}:')
+    dados_do_restaurante = {'nome':nomeRestaurante, 'categoria': categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante)#Adicionar informação a lista
     print(f'O restaurante {nomeRestaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
 
 def lista_restaurante(): #função responsável por mostrar os restaurantes cadastrados no nosso sistema.
     exibir_substitulo('Listando todos os restaurantes')
-    for restaurante in restaurantes:
-        print(f'.{restaurante}')
+    for restaurante in restaurantes: #atualizado para exibir com melhor organização as caracteristicas de cada restaurante.
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante ['categoria']
+        ativo = restaurante['ativo']
+        print(f'- {nome_restaurante} | {categoria} | {ativo}')
     voltar_ao_menu_principal()
 
+def alternar_ativacao():#função criada para alterarmos a ativação dos restaurantes cadastrados
+    exibir_substitulo('Alterando o estado do restaurante')
+    nomeRestaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes: #condição criada para validar a situação atual do restaurante selecionado
+        if nomeRestaurante == restaurantes['nome']: #Se o nome do restaurante digitado for igual ao nome do restuarante cadastrado, executa esse bloco
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            #Se o restaurante for ativado, exibe a primeira mensagem, caso contrário exibe a segunda mensagem
+            mensagem = f'O restaurante {nomeRestaurante} foi ativado com sucesso' if restaurante ['ativo'] else f'O Restaurante {nomeRestaurante} foi desativado com sucesso.'
+            print(mensagem)
+    if not restaurante_encontrado: #caso o nome do restaurante não esteja cadastrado ou seja digitado errado
+        print('O restaurante não foi encontrado.')            
+            
+    voltar_ao_menu_principal()
+    
+    
 def escolheOpcao():#Função que irá mostrar a opção que voce escolheu
 
     try: #Está pedindo para tentar uma das opções do sistema, caso não consiga nenhuma ele irá ativar a condição except.
@@ -50,7 +76,7 @@ def escolheOpcao():#Função que irá mostrar a opção que voce escolheu
         elif op == 2:
             lista_restaurante()
         elif op == 3:
-            print('Ativar restaurante')
+            alternar_ativacao()
         elif op == 4:
             finalizar_app()
         else:
